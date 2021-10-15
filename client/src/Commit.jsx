53d4af83b3b5e9e6d422158aa5260ec07199ad36
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-10-15 13:52:51 -0700

    Style commit section

--- client/src/Commit.jsx
+++ client/src/Commit.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 let Commit = ({commit})=>{
 
   return (
-    <div>
-      <h3>{commit.title}</h3>
-      <p>By: {commit.committer}</p>
-      <span>{commit.dateCreated}</span>
-      <span>{commit.commitHash}</span>
+    <div className = 'commit-list-item flex-column'>
+      <h3 className = 'commit-title'>{commit.title}</h3>
+      <div className = ' commit-row2 flex-row'>
+        <span>By: {commit.committer}</span>
+        <span>{commit.dateCreated}</span>
+      </div>
+      <span className = 'hash'>Commit: {commit.commitHash}</span>
     </div>
   )
 }
--- client/src/RepoListItem.jsx
+++ client/src/RepoListItem.jsx
@@ -20,29 +20,35 @@ let RepoListItem = ({item})=>{
     return (
     <div className = 'repo-list-item'>
       <div className = 'flex-column' onClick ={()=>handleRepoClick(item.owner, item.name)}>
-      <div  className='flex-row repo-row1'>
+      <div  className ='flex-row repo-row1'>
         <h3 className = 'repo-name'>{item.name}</h3>
         <span className = 'language'>{item.language}</span>
       </div>
       <p className = 'repo-description repo-row2'>{item.description}</p>
-      <div className='flex-row repo-row3'>
-        <span className = 'star-count'>star count: {item.starCount}</span>
-        <span className = 'forks-count'>forks: {item.forkCount}</span>
-        <span className ='date-created'>Created: {item.dateCreated}</span>
+      <div className ='flex-row repo-row3'>
+        <span className = 'star-count'><i className ="fa fa-star" aria-hidden = "true"></i> {item.starCount}</span>
+        <span className = 'forks-count'><i className = "fa fa-code-branch" aria-hidden= "true"></i> {item.forkCount}</span>
+        <span className = 'date-created'>Created: {item.dateCreated}</span>
       </div>
 
       </div>
       <Modal
-        isOpen ={modalIsOpen}
+        isOpen = {modalIsOpen}
         contentLabel = 'Recent Commits'
-        onRequestClose= {()=> setModalIsOpen(false)}
-        appElement={document.getElementById('app')}
-        style={{
-          'overlay': {'background':'grey'},
-          'content': {'color':'black', 'width': '600px', 'margin':'auto'} }}>
-        <span style={{'float': 'right', 'fontSize': '150%'}} onClick= {()=> setModalIsOpen(false)}>&#10006;</span>
-        <h2>Recent Commits for {item.name}</h2>
-        {commits.map((commit, i)=> <Commit key= {i} commit= {commit}/>)}
+        onRequestClose = {()=> setModalIsOpen(false)}
+        appElement = {document.getElementById('app')}
+        style = {{
+          'overlay': {'background':'#a1a1a1'},
+          'content': {'box-shadow': '0 4px 20px rgb(0 0 0 / 30%)', 'overflow': 'hidden', 'border': 'none', 'padding': '0', 'color':'black', 'width': '600px', 'font-family': 'Manrope', 'margin':'auto'} }}>
+        <div className='modal-top'>
+          <span className = 'close-icon' onClick= {()=> setModalIsOpen(false)}><i className = "far fa-times-circle"></i></span>
+          <h2 className = 'modal-name'>Recent Commits for {item.name}</h2>
+        </div>
+        <div className = 'commit-list'>
+          {commits.map((commit, i)=> <Commit key= {i} commit= {commit}/>)}
+        </div>
+
+
         </Modal>
     </div>
     )
