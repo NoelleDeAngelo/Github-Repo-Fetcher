55264804aaa92f67df421335acd9efe6270461ca
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-10-15 14:14:14 -0700

    Fix orginization not updadting when using enter to search

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,6 +44,7 @@ let App = ()=>{
 
   let handleKeyPress = (e)=>{
     if (e.key === 'Enter'){
+      setOrg(searchFor);
       handleGetRepos(searchFor)
     }
   }
@@ -51,17 +52,17 @@ let App = ()=>{
 
 
     return (
-    <div className= 'app'>
-      <h1 className= 'title'>Github Repo Fetcher</h1>
-      <p className= 'tag-line'>Easily find and view Repositories from all of your favorite Github Orginizations</p>
-      <div className= 'search-container'>
-        <input placeholder= 'Find Repositories For...' id= 'search' className='search-input' onKeyPress= {handleKeyPress} type = 'text' value = {searchFor} onChange = {(e)=>{handleSearchInput(e)}}></input>
-        <i onClick= {()=>{handleGetRepos(searchFor); setOrg(searchFor)}}className="fa fa-search" aria-hidden="true"></i>
+    <div className = 'app'>
+      <h1 className = 'title'>Github Repo Fetcher</h1>
+      <p className = 'tag-line'>Easily find and view Repositories from all of your favorite Github Orginizations</p>
+      <div className = 'search-container'>
+        <input placeholder = 'Find Repositories For...' id = 'search' className ='search-input' onKeyPress = {handleKeyPress} type = 'text' value = {searchFor} onChange = {(e)=>{handleSearchInput(e)}}></input>
+        <i onClick = {()=>{handleGetRepos(searchFor); setOrg(searchFor)}}className="fa fa-search" aria-hidden="true"></i>
       </div>
       <div className='poss-orgs-container'>
         {possibleOrgs.map((possOrg, i)=> (<p className='poss-orgs' key= {i} onClick={()=> { handleGetRepos(possOrg); setOrg(possOrg)}}>{possOrg}</p>))}
       </div>
-      {loading ? <PropagateLoader color = '#5dcfb4' loading= {loading} size= {30} css= {css`display: block; margin: auto;`}/> : <RepoList org= {org} repos= {repos}/>}
+      {loading ? <PropagateLoader color = '#5dcfb4' loading= {loading} size= {30} css= {css`display: block; margin: auto;`}/> : <RepoList org = {org} repos= {repos}/>}
     </div>
     )
 
